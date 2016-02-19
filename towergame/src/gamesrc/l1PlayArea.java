52edb22954aaeb16ea454eb24d433080df31fd38
michaelngo0917 <786714@seq.org>
2016-02-18 21:27:51 -0800

    Lesson 2 finished

---
+++ towergame/src/gamesrc/l1PlayArea.java
@@ -0,0 +1,18 @@
+package gamesrc;
+
+import java.awt.image.BufferedImage;
+
+import jgame.GContainer;
+import jgame.GSprite;
+import jgame.ImageCache;
+
+public class l1PlayArea extends GContainer {
+	
+	public l1PlayArea(){
+		setSize(800, 600);
+		BufferedImage bg = ImageCache.forClass(TowerGame.class).get("imgres.jpg");
+		GSprite gs = new GSprite(bg);
+		setBackgroundSprite(gs);
+	}
+
+}
