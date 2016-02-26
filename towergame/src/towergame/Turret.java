f3e62dbf8e65b048cd32b7c97c1eb7907c88ae28
790301 <790301@seq.org>
2016-02-26 08:51:21 -0800

    Lesson 5 complete except for resources

---
+++ towergame/src/towergame/Turret.java
@@ -0,0 +1,11 @@
+package towergame;
+
+import java.awt.Image;
+import jgame.GSprite;
+
+public abstract class Turret extends GSprite {
+	
+	public Turret(Image image) {
+		super(image);
+	}
+}
---
+++ towergame/src/towergame/TurretOne.java
@@ -0,0 +1,9 @@
+package towergame;
+
+import java.awt.Image;
+
+public class TurretOne extends Turret {
+	public TurretOne(Image image) {
+		super(image);
+	}
+}
